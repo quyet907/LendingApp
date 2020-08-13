019d59fb42ea37babcfc83ad8b9e82ff7a3e7a1f
Lương lgnvpr <lgnvpr@gmail.com>
2020-08-13 07:03:39 +0700

    vanluong

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, StyleSheet, Navigator } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
