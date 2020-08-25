5e7d000660d244c862308e1f479a733470c470c6
lgnv <lgnvpr@gmail.com>
2020-08-25 11:10:14 +0700

    ahihi

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'module: react-native-dotenv'],
     
   };
 };
