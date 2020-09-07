f69e813fea497f389d154b4349e9012b27932f3b
lgnvpr <lgnvpr@gmail.com>
2020-09-07 07:33:04 +0700

    push cho vui

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -117,6 +117,7 @@ export class UserService {
                 }
             })
             .catch(err => {
+                console.log(err);
                 return "error with server"
             })
     }
